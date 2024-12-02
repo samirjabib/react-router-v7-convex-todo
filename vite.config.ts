663a95974255830168044b1243a5dadb555f9adf
samirjabib <samirdev9669@gmail.com>
2024-12-01 20:19:55 -0500

    update for deploy on vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import autoprefixer from "autoprefixer";
 import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { resolve } from 'path';
 
 export default defineConfig({
   css: {
@@ -11,4 +12,13 @@ export default defineConfig({
     },
   },
   plugins: [reactRouter(), tsconfigPaths()],
+  build: {
+    outDir: 'dist'
+  },
+  resolve: {
+    alias: {
+      '~': resolve(__dirname, './app')
+    }
+  }
+
 });
